82756f59bdaba0bae056163b1e022cc92cbbf971
Luis-Fernando <l_fp9999@hotmail.com>
2023-06-07 10:28:07 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,68 @@
+import 'dart:io';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container(),
-      ),
-    ),
+    Myapp(),
   );
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          title: Center(
+            child: Text('My Card'),
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 40.0,
+                  foregroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/105455585?v=4'),
+                ),
+              ),
+              Text(
+                'Luís Fernando',
+                style: GoogleFonts.pacifico(
+                  textStyle: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                'Desenvolvedor Junior',
+                style: GoogleFonts.sourceSansPro(
+                  textStyle: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5),
+                ),
+              ),
+              Text(
+                '________________________________',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                child: Text(''),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
